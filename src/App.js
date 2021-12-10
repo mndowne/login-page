cae60ee642e9ebe607dde007be5da81aee8e16e4
Martin Downes <mndowne@gmail.com>
2021-12-10 12:47:29 -0600

    added useEffect to check if email/password entry is valid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
+  useEffect(() => {
+
+      const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+      if (storedUserLoggedInInformation === '1') {
+        setIsLoggedIn(true);
+      }
+    
+  }, []);  //No dependencies so it only run once. the dependencies never update because there is none
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
