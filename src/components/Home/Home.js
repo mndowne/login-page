489ae0e84df127b7e53dc4b33e367419248ef882
Martin Downes <mndowne@gmail.com>
2021-12-11 15:07:10 -0600

    moved the logout and login handlers to the auth-context file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React , { useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
+import AuthContext from '../../store/auth-context';
+import Button from '../UI/Button/Button';
 
 const Home = (props) => {
-  return (
-    <Card className={classes.home}>
-      <h1>Welcome back!</h1>
-    </Card>
-  );
+
+    const authCtx = useContext(AuthContext);
+
+    return (
+        <Card className={classes.home}>
+            <h1>Welcome back!</h1>
+            <Button onClick={authCtx.logoutHandler}>Logout</Button>
+        </Card>
+    );
 };
 
 export default Home;
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    logoutHandler: () => {},
+    loginHandler: (email, password) => {},
+    
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);  //No dependencies so it only run once. the dependencies never update because there is none
+
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    }
+    
+    return <AuthContext.Provider
+        value={{
+            isLoggedIn: isLoggedIn,
+            loginHandler: loginHandler,
+            logoutHandler: logoutHandler,
+        }}
+    >
+        {props.children}
+    </AuthContext.Provider>;
+}
 
 export default AuthContext;
